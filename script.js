89d2796143ade2b776102c5e35945736c4fa4e4a
Adarsh kashyap <153892536+kashyapadarsh13@users.noreply.github.com>
2025-04-07 19:08:35 +0530

    Update script.js
    new changes in js and updated to the script.js file

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const textArea = document.getElementById("textarea")
 const totalChar = document.getElementById("total-char")
 const remainChar = document.getElementById("remaining-char")
+const bdc=document.getElementsByClassName("container")
 
 
 textArea.addEventListener("keyup", () => {
     updateCounter()
+    changeColor()
 })
 
 updateCounter()
-
+changeColor()
 function updateCounter(){
     totalChar.innerText = textArea.value.length
     remainChar.innerText =  textArea.getAttribute("maxLength") - textArea.value.length
+}
+function changeColor(){
+    if(textArea.value.length == 100){
+    let color =["red","blue","green","purple","orange"]
+    let randomColor = Math.floor(Math.random()*color.length)
+    bdc[0].style.backgroundColor = color[randomColor]
+    bdc[0].style.transition = "3s"
+    }
+    else{
+        bdc[0].style.backgroundColor = "white"
+        bdc[0].style.color = "black"
+    }
 }
